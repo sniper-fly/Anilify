8964dcc634a5520b2b85d3d7926bef5e6fa89a63
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-22 14:08:55 +0900

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ApolloWrapper>
-          {children}
-        </ApolloWrapper>
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
