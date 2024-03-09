79190fd7538a6fcdd1663ad62c522d5fe75ca18d
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-09 15:24:11 +0900

    typeの可読性向上

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,19 @@
-export type AnimeSong = [
-  {
-    id: number; // anime theme id
-    external_id: number; // 画面に要素を並べる時、keyとして使う
-    site: string; // AniList or MyAnimeList
-    name: string; // anime title
-    animethemes: [
-      {
-        // id: number;
-        title: string; // song title
-        type: string; // OP or ED
-        slug: string; // song slug (ex. OP1, ED2)
-        artists: string[];
-        spotify?: TrackInfo[];
-      }
-    ];
-  }
-];
+export type Anime = {
+  id: number; // anime theme id
+  external_id: number; // 画面に要素を並べる時、keyとして使う
+  site: string; // AniList or MyAnimeList
+  name: string; // anime title
+  animethemes: ThemeSong[];
+};
+
+export type ThemeSong = {
+  // id: number;
+  title: string; // song title
+  type: string; // OP or ED
+  slug: string; // song slug (ex. OP1, ED2)
+  artists: string[];
+  spotify?: TrackInfo[];
+};
 
 export type TrackInfo = {
   uri: string;
@@ -31,7 +29,11 @@ export type TrackInfo = {
   available_markets: string[]; // ユーザーのprofile.countryと一致しない場合は表示しない
 };
 
-export type SearchCache = {
+export type DynamoSearchCache = {
   query: string;
   tracks: TrackInfo[];
+};
+
+export type SearchResult = {
+  [key: string]: TrackInfo[];
 };
