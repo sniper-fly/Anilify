3fce6035e9e7011c58abd464c032919c2acb9712
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-14 15:27:55 +0900

    User_Anime_ListQueryから安全にMediumを取り出す関数を作成

---
+++ src/lib/extractMedium.ts
@@ -0,0 +1,22 @@
+import { User_Anime_ListQuery } from "@/graphql/graphql";
+import { Medium } from "@/types";
+
+export function extractMedium(data: User_Anime_ListQuery): Medium {
+  const medium: Medium = [];
+
+  const lists = data.MediaListCollection?.lists;
+  if (!lists) return medium;
+  // const medium = lists
+  //   .map((list) => list?.entries?.map((entry) => entry?.media) ?? [])
+  //   .flat(1); // 空白エントリーはflatで削除される
+  for (const list of lists) {
+    if (!list?.entries) continue;
+    for (const entry of list.entries) {
+      if (!entry) continue;
+      const media = entry.media;
+      if (!media) continue;
+      medium.push(media);
+    }
+  }
+  return medium;
+}
