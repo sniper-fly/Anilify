fd362646f9a04c683cd9533d189cbe9de1ad618c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-18 15:34:33 +0900

    コメント間違いを修正

--- next-app/docker/entrypoint.sh
+++ next-app/docker/entrypoint.sh
@@ -6,15 +6,15 @@ gid=$(stat -c "%g" .)
 
 if [ "$uid" -ne 0 ]; then
   if [ "$(id -g $USER)" -ne $gid ]; then
-    # builder ユーザーの gid とカレントディレクトリの gid が異なる場合、
-    # builder の gid をカレントディレクトリの gid に変更し、ホームディレクトリの
+    # node ユーザーの gid とカレントディレクトリの gid が異なる場合、
+    # node の gid をカレントディレクトリの gid に変更し、ホームディレクトリの
     # gid も正常化する。
     getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
     chgrp -R $gid $HOME
   fi
   if [ "$(id -u $USER)" -ne $uid ]; then
-    # builder ユーザーの uid とカレントディレクトリの uid が異なる場合、
-    # builder の uid をカレントディレクトリの uid に変更する。
+    # node ユーザーの uid とカレントディレクトリの uid が異なる場合、
+    # node の uid をカレントディレクトリの uid に変更する。
     # ホームディレクトリは usermod によって正常化される。
     usermod -u $uid $USER
   fi
@@ -27,7 +27,7 @@ if [ $node_module_owner -ne $uid ] || [ $node_module_group -ne $gid ] ; then
   chown -R $USER:$USER ./node_modules
 fi
 
-# このスクリプト自体は root で実行されているので、uid/gid 調整済みの builder ユーザー
+# このスクリプト自体は root で実行されているので、uid/gid 調整済みの node ユーザー
 # として指定されたコマンドを実行する。
 # tini が正しく終了できるように、execは利用しない
 su-exec $USER "$@"
