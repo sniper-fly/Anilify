529dc0216c27fc884564552945ddcf6bc3e2c070
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-02 12:09:02 +0900

    DB改名

--- cloud/dynamodb.tf
+++ cloud/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "example" {
-  name           = "AniTunesSearchCache"
+  name           = "AniTunesSpotifySearchCache"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "query"
 
