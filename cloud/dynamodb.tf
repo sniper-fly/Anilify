4f1900e9ac32bef1763c86ec9227e47c7d686bed
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-04 11:01:52 +0900

    change resource name

--- cloud/dynamodb.tf
+++ cloud/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "example" {
+resource "aws_dynamodb_table" "anitunes_spotify_search_cache" {
   name           = "AniTunesSpotifySearchCache"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "query"
