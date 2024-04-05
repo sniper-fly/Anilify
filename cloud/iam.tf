95999b44d181495e2cee75cba95fe1c1dd80aa6c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-05 11:39:50 +0900

    iamアクセスキーの作成と、アクセスキーを自動で更新できるスクリプト

--- cloud/iam.tf
+++ cloud/iam.tf
@@ -2,3 +2,18 @@ resource "aws_iam_user" "anitunes" {
   name = "anitunes_dev"
   path = "/system/"
 }
+
+resource "aws_iam_access_key" "anitunes" {
+  user = aws_iam_user.anitunes.name
+  pgp_key = "keybase:miroscular"
+}
+
+output "anitunes_access_key_id" {
+  description = "The access key id"
+  value       = aws_iam_access_key.anitunes.id
+}
+
+output "anitunes_access_key" {
+  description = "The gpg encrypted secret access key"
+  value       = aws_iam_access_key.anitunes.encrypted_secret
+}
