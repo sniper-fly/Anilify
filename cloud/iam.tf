26789f27a3ff86e462e85577637fb829373728f0
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-05 11:51:45 +0900

    dynamodbfullaccess権限のポリシー付与

--- cloud/iam.tf
+++ cloud/iam.tf
@@ -7,13 +7,23 @@ resource "aws_iam_access_key" "anitunes" {
   user = aws_iam_user.anitunes.name
   pgp_key = "keybase:miroscular"
 }
+data "aws_iam_policy" "DynamoDBFullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonDynamoDB"
+}
+
+resource "aws_iam_user_policy_attachment" "anitunes" {
+  user       = aws_iam_user.anitunes.name
+  policy_arn = data.aws_iam_policy.DynamoDBFullAccess.arn
+}
 
 output "anitunes_access_key_id" {
   description = "The access key id"
   value       = aws_iam_access_key.anitunes.id
+  sensitive = true
 }
 
 output "anitunes_access_key" {
   description = "The gpg encrypted secret access key"
   value       = aws_iam_access_key.anitunes.encrypted_secret
+  sensitive = true
 }
