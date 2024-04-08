3ecd939ddeba63e49d355ecff66b574ae83ce940
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-08 17:37:42 +0900

    spotify検索時にsearchResult型を利用してjsonにそもそものqueryの重複を無くす

--- next-app/src/app/hoge/searchSongs.ts
+++ next-app/src/app/hoge/searchSongs.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { exampleAnimeInfo } from "@/example/exampleObjects";
+import { SearchResult } from "@/types";
 import { AccessToken, SpotifyApi } from "@spotify/web-api-ts-sdk";
 
 import { writeFile } from "fs/promises";
@@ -16,11 +17,13 @@ export default async function searchSongs(token: AccessToken) {
     process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!,
     token
   );
-  // とりあえず 30件 のタイトルをSpotifyで検索し結果を取得する
+  // とりあえず 数件 のタイトルをSpotifyで検索し結果を SearchResult型に変換
+  const searchResult: SearchResult = {};
   for (const theme of themes.slice(0, 10)) {
+    console.log("searching: ", theme.title)
     const items = await api.search(theme.title, ["track"]);
-    console.log(items.tracks.items);
-    theme.spotify = items.tracks.items.map((item) => ({
+
+    searchResult[theme.title] = items.tracks.items.map((item) => ({
       uri: item.uri,
       name: item.name,
       artists: item.artists.map((artist) => ({
@@ -35,10 +38,12 @@ export default async function searchSongs(token: AccessToken) {
       duration_ms: item.duration_ms,
       available_markets: item.available_markets,
     }));
+
+    theme.spotify = searchResult[theme.title];
   }
   console.log("====================================")
 
-  // themes.slice(0, 15) の情報をjsonでファイルに出力する
-  const jsonData = JSON.stringify(themes.slice(0, 15));
-  await writeFile("public/themes.json", jsonData, "utf-8")
+  const jsonData = JSON.stringify(searchResult);
+  await writeFile("src/example/searchResult.json", jsonData, "utf-8")
+  console.log("json output done!")
 }
