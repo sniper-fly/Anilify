50b127f24118dc8ae696e50b5d2f47fa8b5f5dbf
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-13 13:50:07 +0900

    chunkarrayの安全性の向上

--- next-app/src/lib/utils.ts
+++ next-app/src/lib/utils.ts
@@ -6,6 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function chunkArray<T>(array: T[], chunkSize: number): T[][] {
+  if (chunkSize <= 0) return [];
+
   const chunks: T[][] = [];
   for (let i = 0; i < array.length; i += chunkSize) {
     chunks.push(array.slice(i, i + chunkSize));
