c3e13d3253a52a43070784f71f9d38a4b52dfb3e
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-16 11:42:39 +0900

    flexを使って要素整列

--- next-app/src/app/generator/animeRow.tsx
+++ next-app/src/app/generator/animeRow.tsx
@@ -18,39 +18,35 @@ export default function AnimeRow({
       {medium.map((m) => {
         if (!m) return null;
         return (
-          <div key={m.id} className="mb-4">
-            <Image
-              src={m?.coverImage?.large} // add fallback image
-              alt={m?.title?.romaji || "unknown"}
-              width={200}
-              height={340}
-            />
-            <p>{m?.title?.native}</p>
-            <p>{m?.title?.english}</p>
+          <div key={m.id} className="flex flex-row my-5">
+            <div className="ml-3">
+              <Image
+                src={m?.coverImage?.large} // add fallback image
+                alt={m?.title?.romaji || "unknown"}
+                width={200}
+                height={340}
+              />
+            </div>
+            <div className="ml-3">
+              <p>{m?.title?.native}</p>
+              <p>{m?.title?.english}</p>
+            </div>
 
             {/* animetheme apiと通信中はloading iconを表示する */}
             {isAnimeThemeLoading && !animeInfo[m.id] && <p>Loading...</p>}
 
             {/* m.idを使って、animeInfoから情報を取得する */}
-            {animeInfo[m.id]?.animethemes.map((theme) => {
-              return (
-                <div key={theme.title + theme.slug}>
-                  <p>===================</p>
-                  <p>{theme.title}</p>
-                  <p>{theme.slug}</p>
-                  <p>{theme.artists.join(", ")}</p>
-                  {theme.spotify?.map((track) => {
-                    return (
-                      <div key={track.uri}>
-                        <p>{track.name}</p>
-                        <p>{track.artists.map((a) => a.name).join(", ")}</p>
-                        <img src={track.image} alt={track.name} />
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
+            <div>
+              {animeInfo[m.id]?.animethemes.map((theme) => {
+                return (
+                  <div key={theme.title + theme.slug} className="flex flex-row ml-3">
+                    <p>{theme.slug}</p>
+                    <p>{theme.title}</p>
+                    <p>{theme.artists.join(", ")}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         );
       })}
