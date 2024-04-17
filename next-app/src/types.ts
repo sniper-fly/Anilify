eeb96639d4455d705fe2e68c298f06da0808965b
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-17 12:58:53 +0900

    再帰的型定義を実用化

--- next-app/src/types.ts
+++ next-app/src/types.ts
@@ -1,3 +1,5 @@
+import { User_Anime_ListQuery } from "./graphql/graphql";
+
 export type AnimeInfo = {
   [external_id: string]: {
     // AniList/MAL id
@@ -35,76 +37,6 @@ export type SearchResult = {
   [key: string]: TrackInfo[];
 };
 
-export type Medium = Array<{
-  __typename?: "Media";
-  id: number;
-  title?: {
-    __typename?: "MediaTitle";
-    native?: string | null;
-    romaji?: string | null;
-    english?: string | null;
-  } | null;
-  coverImage?: {
-    __typename?: "MediaCoverImage";
-    extraLarge?: string | null;
-    large?: string | null;
-  } | null;
-} | null>;
-
-type User_Anime_ListQuery = {
-  __typename?: "Query";
-  MediaListCollection?: {
-    __typename?: "MediaListCollection";
-    lists?: Array<{
-      __typename?: "MediaListGroup";
-      entries?: Array<{
-        __typename?: "MediaList";
-        media?: {
-          __typename?: "Media";
-          id: number;
-          title?: {
-            __typename?: "MediaTitle";
-            native?: string | null;
-            romaji?: string | null;
-            english?: string | null;
-          } | null;
-          coverImage?: {
-            __typename?: "MediaCoverImage";
-            extraLarge?: string | null;
-            large?: string | null;
-          } | null;
-        } | null;
-      } | null> | null;
-    } | null> | null;
-  } | null;
-};
-
-type extractTypeNameFrom<T, __typename> = T extends
-  | {
-      [key in string]?: infer U;
-    }
-  | Array<infer U>
-  ? U extends { [key in string]?: infer V }
-    ? V extends Array<infer W>
-      ? W extends { [key in string]?: infer X }
-        ? X extends Array<infer Y>
-          ? Y extends { [key in string]?: infer Z }
-            ? Z extends { __typename?: __typename }
-              ? Z
-              : never
-            : never
-          : never
-        : never
-      : never
-    : never
-  : never;
-
-// "Media" に到達するまで、型を再帰的に探索する
-// 課題: infer で取得した型に__typename、が混ざってしまうので、型の再帰的な探索ができない
-// exclude で除外する型を指定することで、再帰的な探索を行えないだろうか？
-type md = extractTypeNameFrom<User_Anime_ListQuery, "Media">;
-type mds = Array<md>;
-
 type extractTypeName<T, __typename> = T extends
   | {
       [key in string]?: infer U;
@@ -115,5 +47,4 @@ type extractTypeName<T, __typename> = T extends
     : extractTypeName<U, __typename>
   : never;
 
-type md2 = extractTypeName<User_Anime_ListQuery, "Media">;
-type mds2 = Array<md2>;
+export type Medium = extractTypeName<User_Anime_ListQuery, "Media">[];
