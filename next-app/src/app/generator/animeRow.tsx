e34613d818c9122ed96b35d448e196be49efe3e6
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-18 11:23:06 +0900

    class名整列

--- next-app/src/app/generator/animeRow.tsx
+++ next-app/src/app/generator/animeRow.tsx
@@ -15,7 +15,7 @@ export default function AnimeRow({
 }: Props) {
   return (
     <>
-      <div className="flex flex-row pl-3 gap-3 sticky top-0 bg-slate-600 text-white text-center">
+      <div className="sticky top-0 flex flex-row gap-3 bg-slate-600 pl-3 text-center text-white">
         <div className="w-40">Image</div>
         <div className="w-40">Anime title</div>
         <div className="w-16">Type</div>
@@ -25,7 +25,7 @@ export default function AnimeRow({
       {medium.map((m) => {
         if (!m) return null;
         return (
-          <div key={m.id} className="flex flex-row my-5 gap-3 pl-3">
+          <div key={m.id} className="my-5 flex flex-row gap-3 pl-3">
             <div className="w-40">
               <Image
                 src={m?.coverImage?.large} // add fallback image
@@ -38,15 +38,16 @@ export default function AnimeRow({
               <p>{m?.title?.native}</p>
               <p>{m?.title?.english}</p>
             </div>
-
             {/* animetheme apiと通信中はloading iconを表示する */}
             {isAnimeThemeLoading && !animeInfo[m.id] && <p>Loading...</p>}
-
             {/* m.idを使って、animeInfoから情報を取得する */}
             <div>
               {animeInfo[m.id]?.animethemes.map((theme) => {
                 return (
-                  <div key={theme.title + theme.slug} className="flex flex-row gap-3">
+                  <div
+                    key={theme.title + theme.slug}
+                    className="flex flex-row gap-3"
+                  >
                     <p className="w-16">{theme.slug}</p>
                     <p className="w-36">{theme.title}</p>
                     <p className="w-36">{theme.artists.join(", ")}</p>
