dbb60083c71b27af8ad611017552d48425a0e0b9
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-08-06 17:02:10 +0900

    cloudfrontの実装は時間がかかるので一旦コメントアウト

--- cloud/cloudfront.tf
+++ cloud/cloudfront.tf
@@ -1,61 +1,61 @@
-resource "aws_cloudfront_distribution" "example_distribution" {
-  # provider = aws.us-east-1
+# resource "aws_cloudfront_distribution" "example_distribution" {
+#   # provider = aws.us-east-1
 
-  origin {
-    origin_id   = aws_api_gateway_rest_api.example_api_gateway.id
-    domain_name = "${aws_api_gateway_rest_api.example_api_gateway.id}.execute-api.ap-northeast-1.amazonaws.com"
-    origin_path = "/${aws_api_gateway_stage.example_next_api.stage_name}"
+#   origin {
+#     origin_id   = aws_api_gateway_rest_api.example_api_gateway.id
+#     domain_name = "${aws_api_gateway_rest_api.example_api_gateway.id}.execute-api.ap-northeast-1.amazonaws.com"
+#     origin_path = "/${aws_api_gateway_stage.example_next_api.stage_name}"
 
-    custom_origin_config {
-      http_port  = 80
-      https_port = 443
+#     custom_origin_config {
+#       http_port  = 80
+#       https_port = 443
 
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols = [
-        "TLSv1",
-        "TLSv1.1",
-        "TLSv1.2"
-      ]
-    }
-  }
+#       origin_protocol_policy = "https-only"
+#       origin_ssl_protocols = [
+#         "TLSv1",
+#         "TLSv1.1",
+#         "TLSv1.2"
+#       ]
+#     }
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  price_class         = "PriceClass_200"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   price_class         = "PriceClass_200"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_api_gateway_rest_api.example_api_gateway.id
+#   default_cache_behavior {
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = aws_api_gateway_rest_api.example_api_gateway.id
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-    minimum_protocol_version       = "TLSv1.2_2021"
-    ssl_support_method             = "sni-only"
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#     minimum_protocol_version       = "TLSv1.2_2021"
+#     ssl_support_method             = "sni-only"
+#   }
 
-  tags = {
-    Name        = "Example CloudFront Distribution"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Name        = "Example CloudFront Distribution"
+#     Environment = "dev"
+#   }
+# }
