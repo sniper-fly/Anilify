75cc81e53f8270570aed08e1e0bb1479aa404468
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-08-12 14:54:46 +0900

    delete redundant unique constraint

---
+++ next-app/prisma/migrations/20240812055121_delete_unique_on_id/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX `Anime_id_key` ON `Anime`;
+
+-- DropIndex
+DROP INDEX `AnimeTheme_id_key` ON `AnimeTheme`;
---
+++ next-app/prisma/migrations/20240812055424_delete_redundant_unique/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX `AnimeThemeArtists_id_key` ON `AnimeThemeArtists`;
+
+-- DropIndex
+DROP INDEX `StreamingArtists_id_key` ON `StreamingArtists`;
