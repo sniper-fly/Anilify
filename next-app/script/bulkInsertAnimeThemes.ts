daf6d3fc87474281f7fa3e7e2eea3f8bf1ae7040
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-08-13 16:18:44 +0900

    artist名、song, song.titleがnullだった場合にskipできるようにreduceを使って再実装

--- next-app/script/bulkInsertAnimeThemes.ts
+++ next-app/script/bulkInsertAnimeThemes.ts
@@ -1,5 +1,6 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import axios from "axios";
+import * as fs from "fs";
 
 function pp(val: any) {
   console.dir(val, { depth: null });
@@ -7,7 +8,7 @@ function pp(val: any) {
 
 const prisma = new PrismaClient();
 
-const exampleJson = {
+const animeJson = {
   id: 4216,
   name: "High Card Season 2",
   resources: [
@@ -42,25 +43,14 @@ const exampleJson = {
         ],
       },
     },
-    {
-      id: 12689,
-      slug: "ED1",
-      song: {
-        title: "Hakuchuumu",
-        artists: [
-          {
-            id: 972,
-            name: "Raon",
-          },
-        ],
-      },
-    },
   ],
 };
 
-type AnimeThemeJson = typeof exampleJson;
+type AnimeJson = typeof animeJson;
 
 async function main() {
+  const errorLog: any[] = [];
+
   for (let page_num = 1; true; page_num++) {
     const baseUrl = "https://api.animethemes.moe/anime";
     const res = await axios.get(baseUrl, {
@@ -73,7 +63,7 @@ async function main() {
         },
         include: "animethemes.song.artists,resources",
         page: {
-          size: 5,
+          size: 100,
           number: page_num,
         },
       },
@@ -81,27 +71,35 @@ async function main() {
     const rawAnimeJSON = res.data.anime;
     if (rawAnimeJSON.length === 0) break;
 
-    // if (page_num === 1) console.log(rawAnimeJSON);
-    await bulkInsert(rawAnimeJSON);
-    if (page_num === 1) break;
+    await bulkInsert(rawAnimeJSON, errorLog);
   }
+
+  // errorLog をファイルに出力する
+  fs.writeFileSync(`logs/errorLog.json`, JSON.stringify(errorLog, null, 2));
 }
 
-async function bulkInsert(json: AnimeThemeJson[]) {
+async function bulkInsert(json: AnimeJson[], errorLog: any[]) {
   for (const record of json) {
     try {
-      const data = record.animethemes.flatMap((theme) =>
-        theme.song.artists.map((artist) => ({
-          id: artist.id,
-          name: artist.name,
-        })),
+      const data = record.animethemes.reduce(
+        (acc, theme) => {
+          const song = theme.song;
+          if (!song || !song.artists) return acc;
+          for (const artist of song.artists) {
+            acc.push({
+              id: artist.id,
+              name: artist.name,
+            });
+          }
+          return acc;
+        },
+        [] as Array<{ id: number; name: string }>,
       );
 
       await prisma.animeThemeArtist.createMany({
         data: data,
         skipDuplicates: true,
       });
-
       await prisma.anime.create({
         data: {
           id: record.id,
@@ -115,23 +113,45 @@ async function bulkInsert(json: AnimeThemeJson[]) {
             ?.external_id,
           title: record.name,
           animeThemes: {
-            create: record.animethemes.map((theme) => {
-              const song = theme.song;
-              return {
-                id: theme.id,
-                title: song.title,
-                slug: theme.slug,
+            create: record.animethemes.reduce(
+              (accumulator, theme) => {
+                const song = theme.song;
+                // songやsong.titleがnullでない場合にのみ処理を続行
+                if (!song || !song.title) return accumulator;
+                accumulator.push({
+                  id: theme.id,
+                  title: song.title,
+                  slug: theme.slug,
+                  artists: {
+                    connect: song.artists?.map((artist) => ({
+                      id: artist.id,
+                    })),
+                  },
+                });
+                return accumulator;
+              },
+              [] as Array<{
+                id: number;
+                title: string;
+                slug: string;
                 artists: {
-                  connect: song.artists.map((artist) => ({
-                    id: artist.id,
-                  })),
-                },
-              };
-            }),
+                  connect: Array<{ id: number }>;
+                };
+              }>,
+            ),
           },
         },
       });
     } catch (e) {
+      const err = e as Error;
+      errorLog.push({
+        record,
+        error: {
+          error: err,
+          message: err.message,
+          stack: err.stack,
+        },
+      });
       console.error(e);
       pp(record);
     }
