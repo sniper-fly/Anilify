b63eba928964e10e5683b0a89ccf7cb503e41658
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-09-13 18:10:15 +0900

    ecr privateに戻す

--- cloud/lambda.tf
+++ cloud/lambda.tf
@@ -1,9 +1,9 @@
-resource "aws_ecrpublic_repository" "example_lambda_repo" {
-  repository_name = "example_lambda_repo"
+resource "aws_ecr_repository" "example_lambda_repo" {
+  name = "example_lambda_repo"
 }
 
 resource "terraform_data" "push_image" {
-  depends_on = [aws_ecrpublic_repository.example_lambda_repo]
+  depends_on = [aws_ecr_repository.example_lambda_repo]
 
   provisioner "local-exec" {
     // ローカルのスクリプトを呼び出す
@@ -13,7 +13,7 @@ resource "terraform_data" "push_image" {
     environment = {
       AWS_REGION     = var.aws_region
       AWS_ACCOUNT_ID = local.aws_account_id
-      REPO_URL       = aws_ecrpublic_repository.example_lambda_repo.repository_uri
+      REPO_URL       = aws_ecr_repository.example_lambda_repo.repository_url
     }
   }
 }
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "example_lambda_repo_function" {
 
   function_name = "exampleLambdaRepo"
   package_type  = "Image"
-  image_uri     = "${aws_ecrpublic_repository.example_lambda_repo.repository_uri}:latest"
+  image_uri     = "${aws_ecr_repository.example_lambda_repo.repository_url}:latest"
   role          = aws_iam_role.example_lambda_iam.arn
   timeout       = 30
   memory_size   = 256
